887846fdd001aeb197ab44e560f2271eebc43171
Mario García Valdez <mariosky@gmail.com>
2023-12-06 17:09:38 +0000

    error al concatenar la variable de entorno

--- src/Notify.js
+++ src/Notify.js
@@ -6,7 +6,7 @@ const EventSourceComponent = ({image_url, image_name}) => {
     useEffect(() => {
         // Create a new EventSource instance
         console.log(image_name);
-        const eventSource = new EventSource(`http://${process.env.REACT_APP_BACKEND_URL}/events/${image_name.replace("original/", "")}`);
+        const eventSource = new EventSource(`${process.env.REACT_APP_BACKEND_URL}/events/${image_name.replace("original/", "")}`);
 
         // Handle a message event
         eventSource.onmessage = (event) => {
